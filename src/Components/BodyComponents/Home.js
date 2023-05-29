563e6dbbc0ce76519dc760b381ffae3b508014f7
IsaacKusi <kusiisaacimpact@yahoo.com>
2023-05-29 13:39:58 +0000

    hamburger

--- src/Components/BodyComponents/Home.js
+++ src/Components/BodyComponents/Home.js
@@ -5,10 +5,10 @@ const Home = () => {
     return <>
         <main className='home-animation '>
             <div className="container py-12 ">
-                <p className='text-white text-6xl font-bold mt-36 font-sans'>FULLSTACK DEVELOPER</p>
+                <p className='text-white text-3xl font-bold mt-36 font-sans md:text-4xl lg:text-6xl'>FULLSTACK DEVELOPER</p>
                 <div className='mt-44 text-white font-sans font-semibold'>
                     <p>Hey! I am Isaac</p>
-                    <p>I build Interactive Websites and web applications</p>
+                    <p>I build Interactive Websites and Web Applications</p>
                     <p>that run across platforms and devices</p>
                 </div>
             </div>
--- src/Components/NavItems/Nav.js
+++ src/Components/NavItems/Nav.js
@@ -1,21 +1,35 @@
 import logo from '../images/IKe.jpg'
 import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa'
+import { useState } from 'react';
+
 
 const Nav = () => {
+    const [showModal, setShowModal] = useState(false)
+
     return <>
-        <main className={`bg-transparent bg-cover bg-center fixed top-5 left-0 right-0 `} >
+        <main className="bg-transparent fixed top-5 left-0 right-0" >
             <div className="container flex justify-between">
                 <div className='flex items-center mt-1'>
                     <img src={logo} alt="" width='40px' height='40px' className='rounded-3xl' />
                     <p className='ml-2 font-semibold text-white text-lg'>Isaac Kusi</p>
                 </div>
-                <div className='flex justify-between items-center font-semibold mt-1 text-white'>
-                  <Link to= '/'> <p className='px-1 h-8 '> Home </p></Link> 
-                  <Link to= '/about'> <p className='ml-1 px-1  h-8'> About </p></Link>
-                  <Link to= '/skills'><p className='ml-1 px-1  h-8'> Skills </p></Link>
-                  <Link to= '/projects'><p className='ml-1 px-1  h-8'> Projects </p></Link>
+                <div className=' hidden md:flex md:justify-between md:items-center font-semibold mt-1 text-white '>
+                    <Link to='/'> <p className='px-1 h-8 '> Home </p></Link>
+                    <Link to='/about'> <p className='ml-1 px-1  h-8'> About </p></Link>
+                    <Link to='/skills'><p className='ml-1 px-1  h-8'> Skills </p></Link>
+                    <Link to='/projects'><p className='ml-1 px-1  h-8'> Projects </p></Link>
                 </div>
+                <FaBars className='text-white  font-semibold mt-3 md:hidden text-xl w-15 cursor-pointer' onClick={()=>setShowModal(!showModal)} />
             </div>
+            {showModal && <div className=' container  flex place-content-end' onClick={()=>setShowModal(!showModal)}>
+                <div className=' mod-cov ml-5 font-semibold mt-1 text-white md:hidden text-center'>
+                    <Link to='/'> <p className='px-1 h-8  '> Home </p></Link>
+                    <Link to='/about'> <p className='ml-1 px-1  h-8 '> About </p></Link>
+                    <Link to='/skills'><p className='ml-1 px-1  h-8 '> Skills </p></Link>
+                    <Link to='/projects'><p className='ml-1 px-1  h-8 '> Projects </p></Link>
+                </div>
+            </div>}
         </main>
     </>
 }
