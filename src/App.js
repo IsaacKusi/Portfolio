ac4ae0c834137348821332cc6d90896b22ac9f1e
IsaacKusi <kusiisaacimpact@yahoo.com>
2023-06-11 17:59:17 +0000

    pics

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Home from "./Components/BodyComponents/Home";
 import About from "./Components/BodyComponents/About";
 import Skills from "./Components/BodyComponents/Skills";
 import Projects from "./Components/BodyComponents/Projects";
-import { Route, Switch} from "react-router-dom";
+import { NavfocusProvider } from "./context/NavFocusContext";
+import { Route, Switch } from "react-router-dom";
 
 
 const App = () => {
 
   return (
     <>
-      <Nav />
+      <NavfocusProvider>
+        <Nav />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
         <Route path='/projects' component={Projects} />
         <Route path='/skills' component={Skills} />
       </Switch>
+    </NavfocusProvider>
     </>
   );
 }
